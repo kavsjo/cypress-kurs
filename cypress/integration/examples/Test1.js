1a760c5d202b8418c630390446951e3c10fc854b
Victor Kavsjö <kavsjo88@hotmail.se>
2021-08-22 21:34:59 +0200

    first commit

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+describe('My First Test suite', () => {
+
+it("My first test case", () => {
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+    cy.get('.search-keyword').type('ca')
+    cy.wait(2000)
+    cy.get('.product:visible').should('have.length', 4)
+})
+
+}  )
+ 
