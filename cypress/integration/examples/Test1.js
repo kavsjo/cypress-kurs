3ff8672f96c229570e6493fa7017344e28204142
Victor Kavsjö <kavsjo88@hotmail.se>
2021-08-23 22:42:53 +0200

    Lsseon 19

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,13 +1,20 @@
 /// <reference types="Cypress" />
 
+function capru() {
+    
+}
+
 describe('My First Test suite', () => {
+    it("My first test case", () => {
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.get('.search-keyword').type('ca')
+        cy.wait(2000)
+        cy.get('.product:visible').should('have.length', 4)
+        // Parent child chaining
+        cy.get('.products').find('.product').should('have.length', 4)
+        
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
 
-it("My first test case", () => {
-    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
-    cy.get('.search-keyword').type('ca')
-    cy.wait(2000)
-    cy.get('.product:visible').should('have.length', 4)
+    })
 })
-
-}  )
  
